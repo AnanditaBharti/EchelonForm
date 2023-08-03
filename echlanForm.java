204cea09c5741483dccfd313b00a419f9d3f7dc0
AnanditaBharti <141313454+AnanditaBharti@users.noreply.github.com>
2023-08-03 20:42:48 +0530

    Add files via upload

---
+++ echlanForm.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class echlanForm {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Input no. of variables:");
+        int var=sc.nextInt();
+        System.out.print("Input no. of equations:");
+        int eq=sc.nextInt();
+        
+        System.out.print("Input entries of the matrix:");
+        int[][] matrix=new int[eq][var];
+        for(int i=0;i<eq;i++)
+        {
+        	for(int j=0;j<var;j++)
+        	{
+        		
+        	}
+        }
+	}
+
+}
